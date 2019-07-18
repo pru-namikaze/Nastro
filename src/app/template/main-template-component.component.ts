e408c5ca934d0fe4b4359caf1c454a7d919c3a34
pranshu <pranshu.goel@lowes.com>
2019-07-18 19:38:11 +0530

    Minor changes.

--- src/app/template/main-template-component.component.ts
+++ src/app/template/main-template-component.component.ts
@@ -1,68 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 import { InfrastructureApiService } from '../services/infrastructure-api.service';
-import { GetReloadDataService } from '../services/get-reload-data.service';
-import UrlDict from './../services/domainUrlDict.json';
 
 @Component({
   selector: 'app-main-template-component',
   templateUrl: './main-template-component.component.html',
   styleUrls: ['./main-template-component.component.css']
 })
+
 export class MainTemplateComponentComponent implements OnInit {
 
-  baseServiceNameList: Array<string>;
-  baseServiceList: Array<string>;
-
-  constructor(public infrastructureApi: InfrastructureApiService, public getReloadData: GetReloadDataService) {
-    this.baseServiceNameList = Object.keys(UrlDict);
-    this.baseServiceList = Object.keys(this.infrastructureApi.ResponceURLDict[this.infrastructureApi.baseServiceName]);
-    infrastructureApi.baseService = this.baseServiceList[0];
+  constructor(public infrastructureApi: InfrastructureApiService) {
   }
 
-  getbaseServiceList() {
-    // tslint:disable-next-line: max-line-length
-    this.infrastructureApi.baseService = (this.infrastructureApi.baseService === '') ? Object.keys(this.infrastructureApi.ResponceURLDict[this.infrastructureApi.baseServiceName])[0] : this.infrastructureApi.baseService;
-    return Object.keys(this.infrastructureApi.ResponceURLDict[this.infrastructureApi.baseServiceName]);
-  }
-
-  getNavbaseServiceList(baseServiceName: string): Array<string> {
-    let baseService: Array<string> = [];
-    for (const index of Object.keys(UrlDict[baseServiceName])) {
-      baseService = [...baseService, UrlDict[baseServiceName][index].Name];
-    }
-    return baseService;
-  }
-
-  Visit(baseServiceName: string, baseService: string): void {
-    console.table([[this.infrastructureApi.baseServiceName, baseServiceName], [this.infrastructureApi.baseService, baseService]]);
-    this.infrastructureApi.baseServiceName = baseServiceName;
-    console.table(this.infrastructureApi);
-    this.infrastructureApi.baseService = baseService;
-
-    this.getReloadData.infrastructureCommonFilter.setFilterPrameters(null);
-
-    this.getReloadData.resetTable();
-
-    this.getReloadData.reloadGetDataGiveToTableMaker(
-      (this.infrastructureApi.GenerateResponseUrl(), this.infrastructureApi.ResponceURLDict),
-      this.infrastructureApi.baseServiceName,
-      this.infrastructureApi.baseService,
-      this.infrastructureApi.QueryPrameters,
-      this.infrastructureApi.QueryPramChoices
-    );
-  }
-
-  toggleNavDropDown(baseServiceName: string, showDropDownFlag: boolean): void {
-    if (showDropDownFlag) {
-      document.getElementById('navbar-list-' + baseServiceName).className = 'nav-item dropdown show';
-      document.getElementById('navbar-dropdown-list-' + baseServiceName).className = 'dropdown-menu show';
-    } else {
-      document.getElementById('navbar-list-' + baseServiceName).className = 'nav-item dropdown';
-      document.getElementById('navbar-dropdown-list-' + baseServiceName).className = 'dropdown-menu';
-    }
-  }
-
-   ngOnInit() {
+  ngOnInit() {
   }
 }
--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -11,6 +11,8 @@ import { NeowsTemplateComponent } from './neows-template/neows-template.componen
 import { FiltersTemplateComponent } from './filters-template/filters-template.component';
 import { TableTemplateComponent } from './table-template/table-template.component';
 import { DonkiTemplateComponent } from './donki-template/donki-template.component';
+import { HeaderTemplateComponent } from './header-template/header-template.component';
+import { FooterTemplateComponent } from './footer-template/footer-template.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,9 @@ import { DonkiTemplateComponent } from './donki-template/donki-template.componen
     NeowsTemplateComponent,
     DonkiTemplateComponent,
     FiltersTemplateComponent,
-    TableTemplateComponent
+    TableTemplateComponent,
+    HeaderTemplateComponent,
+    FooterTemplateComponent
   ],
   imports: [
     BrowserAnimationsModule,
